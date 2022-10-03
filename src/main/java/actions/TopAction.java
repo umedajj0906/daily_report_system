bd71da7a01e9cf30ada241b20622093dce895fe1
梅田 <umedajj@hydrosoken.co.jp>
2022-10-03 16:04:22 +0900

    Lesson17

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -1,17 +1,24 @@
 package actions;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
 
 /**
  * トップページに関する処理を行うActionクラス
  *
  */
 public class TopAction extends ActionBase{
+
+    private ReportService service; //追記
 
 
     /**
@@ -20,8 +27,12 @@ public class TopAction extends ActionBase{
     @Override
     public void process() throws ServletException, IOException{
 
+        service = new ReportService();
+
         //メソッドを実行
         invoke();
+
+        service.close();
 
     }
 
@@ -29,6 +40,25 @@ public class TopAction extends ActionBase{
      * 一覧画面を表示する
      */
     public void index() throws ServletException, IOException{
+
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView loginEmployee = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+
+        //ログイン中の従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得する
+        int page = getPage();
+        List<ReportView> reports = service.getMinePerPage(loginEmployee, page);
+
+
+        //ログイン中の従業員が作成した日報データの件数を取得
+        long myReportsCount = service.countAllMine(loginEmployee);
+
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, myReportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
 
 
         //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
